b6350fe792a121f20978cb1794465834f712c6c9
TWM <15152806231@163.com>
2019-09-03 10:50:23 +0800

    Hero实体类

---
+++ src/com/charactor/Hero.java
@@ -0,0 +1,24 @@
+package com.charactor;
+
+public class Hero {
+	public String name;
+	public float hp;
+	public int damage;
+	
+	public Hero(){
+		
+	}
+	// 增加一个初始化name的构造方法
+	public Hero(String name) {
+		super();
+		this.name = name;
+	}
+	
+	// 重写toString方法
+	@Override
+	public String toString() {
+		return "Hero [name=" + name + "]";
+	}
+	
+	
+}
