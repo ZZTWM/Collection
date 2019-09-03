7d551868bb99e2a7ac04070950dbc6d60f1194b8
TWM <15152806231@163.com>
2019-09-03 14:33:23 +0800

    大龙类

---
+++ src/com/charactor/GiantDragon.java
@@ -0,0 +1,14 @@
+package com.charactor;
+
+public class GiantDragon {
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
