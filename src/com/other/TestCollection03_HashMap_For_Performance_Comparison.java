5fd4b5c1cccb2e06093cfdca06aeb17c979575ed
TWM <15152806231@163.com>
2019-09-10 09:14:48 +0800

    查找内容性能比较

---
+++ src/com/other/TestCollection03_HashMap_For_Performance_Comparison.java
@@ -0,0 +1,74 @@
+package com.other;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.charactor.Hero;
+
+/**
+ * 练习-查找内容性能比较
+ * 准备一个ArrayList其中存放3000000(三百万个)Hero对象，其名称是随机的,格式是hero-[4位随机数]
+ * hero-3229
+ * hero-6232
+ * hero-9365
+ * ...
+ * 因为总数很大，所以几乎每种都有重复，把名字叫做 hero-5555的所有对象找出来
+ * 要求使用两种办法来寻找
+ * 1. 不使用HashMap，直接使用for循环找出来，并统计花费的时间
+ * 2. 借助HashMap，找出结果，并统计花费的时间
+ * @author Administrator
+ *需要注意的是：
+ * Map的key是字符串，英雄的名称
+ * Map的value是List,里面放了名称相同的多个英雄
+ * 如图所示，使用Map查找会快非常多，为什么会快很多呢？ 这就涉及到hashcode 原理，将在后续章节专门详细讲解。
+ */
+public class TestCollection03_HashMap_For_Performance_Comparison {
+    public static void main(String[] args) {
+        List<Hero> hs =new ArrayList<>();
+        System.out.println("初始化开始");
+        for (int i = 0; i < 3000000; i++) {
+            Hero h = new Hero(   "hero-" + random());
+            hs.add(h);
+        }
+        //名字作为key
+        //名字相同的hero，放在一个List中，作为value
+        HashMap<String,List<Hero>> heroMap =new HashMap();
+        for (Hero h : hs) {
+            List<Hero> list= heroMap.get( h.name);
+            if(list==null){
+                list = new ArrayList<>();
+                heroMap.put(h.name, list);
+            }
+            list.add(h);
+        }
+          
+        System.out.println("初始化结束");
+        System.out.println("开始查找");
+        findByIteration(hs);
+        findByMap(heroMap);
+          
+    }
+    private static List<Hero> findByMap(HashMap<String,List<Hero>> m) {
+        long start =System.currentTimeMillis();
+        List <Hero>result= m.get("hero-5555");
+        long end =System.currentTimeMillis();
+        System.out.printf("通过map查找，一共找到%d个英雄，耗时%d 毫秒%n",result.size(),end-start);
+        return result;
+    }
+    private static List<Hero> findByIteration (List<Hero> hs) {
+        long start =System.currentTimeMillis();
+        List<Hero> result =new ArrayList<>();
+        for (Hero h : hs) {
+            if(h.name.equals("hero-5555")){
+                result.add(h);
+            }
+        }
+        long end =System.currentTimeMillis();
+        System.out.printf("通过for查找，一共找到%d个英雄，耗时%d 毫秒%n", result.size(),end-start);
+        return result;
+    }
+    public static int random(){
+        return ((int)(Math.random()*9000)+1000);
+    }	
+}
