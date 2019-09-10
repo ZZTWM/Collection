229bc966940222ff5fdba46f2a76e25ffbde95cb
TWM <15152806231@163.com>
2019-09-10 09:22:47 +0800

    元素不能重复

---
+++ src/com/other/TestCollection03_HashSet.java
@@ -0,0 +1,19 @@
+package com.other;
+
+import java.util.HashSet;
+/**
+ * Set中的元素，不能重复
+ * @author Administrator
+ *
+ */
+public class TestCollection03_HashSet {
+	public static void main(String[] args) {
+		HashSet<String> names = new HashSet<String>();
+		names.add("gareen");
+		System.out.println(names);
+		
+		//第二次插入同样的数据，是插不进去的，容器中只会保留一个
+		names.add("gareen");
+		System.out.println(names);
+	}
+}
