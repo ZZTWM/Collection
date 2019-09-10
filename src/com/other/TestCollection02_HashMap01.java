05c8196a8bcdab0b0fa7cb637cc998aead6810f2
TWM <15152806231@163.com>
2019-09-10 09:01:09 +0800

    HashMap的键值对

---
+++ src/com/other/TestCollection02_HashMap01.java
@@ -0,0 +1,18 @@
+package com.other;
+
+import java.util.HashMap;
+
+/**
+ * HashMap储存数据的方式是—— 键值对
+ * @author Administrator
+ *
+ */
+public class TestCollection02_HashMap01 {
+	public static void main(String[] args) {
+		HashMap<String, String> dictionary = new HashMap<>();
+		dictionary.put("adc", "物理英雄");
+		dictionary.put("apc", "魔法英雄");
+		dictionary.put("t", "坦克");
+		System.out.println(dictionary.get("t"));
+	}
+}
