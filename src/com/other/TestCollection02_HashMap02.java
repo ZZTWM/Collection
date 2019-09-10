2944fd706dc6b7a46e091c354ad1481a8866a92d
TWM <15152806231@163.com>
2019-09-10 09:03:03 +0800

    键不能重复，值可以重复

---
+++ src/com/other/TestCollection02_HashMap02.java
@@ -0,0 +1,12 @@
+package com.other;
+/**
+ * 键不能重复，值可以重复
+ * 对于HashMap而言，key是唯一的，不可以重复的。 
+ * 所以，以相同的key 把不同的value插入到 Map中会导致旧元素被覆盖，只留下最后插入的元素。 
+ * 不过，同一个对象可以作为值插入到map中，只要对应的key不一样
+ * @author Administrator
+ *
+ */
+public class TestCollection02_HashMap02 {
+
+}
