43aca3d1d37b72eacf36bacf83894ef739333902
TWM <15152806231@163.com>
2019-09-20 11:20:16 +0800

    reverse 使List中的数据发生翻转

---
+++ src/com/other/TestCollection04_Collectios01_reverse.java
@@ -0,0 +1,24 @@
+package com.other;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestCollection04_Collectios01_reverse {
+	public static void main(String[] args) {
+		//初始化集合numbers
+		List<Integer> numbers = new ArrayList<>();
+		
+		for (int i = 0; i < 10; i++) {
+			numbers.add(i);
+		}
+		
+		System.out.println("集合中的数据：");
+		System.out.println(numbers);
+		
+		Collections.reverse(numbers);
+		
+		System.out.println("翻转后集合中的数据:");
+		System.out.println(numbers);
+	}
+}
