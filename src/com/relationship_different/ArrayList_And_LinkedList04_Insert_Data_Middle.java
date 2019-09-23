dd390aafafa1e7aca5bd0503ec9e2416baf3eeb8
TWM <15152806231@163.com>
2019-09-23 10:37:58 +0800

    在中间插入数据

--- src/com/relationship_different/ArrayList_And_LinkedList04_Insert_Data_Middle.java
+++ src/com/relationship_different/ArrayList_And_LinkedList04_Insert_Data_Middle.java
@@ -5,7 +5,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 
+ * 在List的中间位置，插入数据，比较ArrayList快，还是LinkedList快，并解释为什么？
+ * 表示在当前容器的一半的位置，插入数据
+ * 在这个位置插入数据
+ * 数组定位很快，插入数据比较慢
+ * 链表定位很慢，插入数据比较快
+ * 最后发现，当总数是10000条的时候，链表定位的总开支要比数组插入的总开支更多，所以最后整体表现，数组会更好。
+ * 如果总数是1000条，结果可能就不一样了。
  */
 public class ArrayList_And_LinkedList04_Insert_Data_Middle {
     public static void main(String[] args) {
